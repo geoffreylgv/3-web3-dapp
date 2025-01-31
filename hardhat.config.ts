8ba17107708b269ce08825aa010deeb4542d1525
Geoffrey Logovi <52314615+geoffreylgv@users.noreply.github.com>
2025-01-31 18:52:08 +0000

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
 import dotenv from "dotenv";
 
 dotenv.config();
